3cd421b930fc50fc8ae6cd916758b4dbe447146c
zichen <zichen1016@qq.com>
2022-07-18 17:00:17 +0800

    使用Swagger显示api

---
+++ 使用Swagger/Swagger显示接口/SwaggerDemo/SwaggerDemo/Controllers/MyController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerDemo.Controllers
+{
+    /// <summary>
+    /// 我的api测试接口
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")] //声明控制器的操作支持 application/json 的响应内容类型
+    public class MyController : ControllerBase
+    {
+        /// <summary>
+        /// 获取数据
+        /// </summary>
+        /// <returns>数组字符串</returns>
+        /// <remarks>
+        /// Sample request(请求样例):
+        ///
+        ///     GET /api/My
+        ///     {
+        ///        
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">返回码：201</response>
+        /// <response code="400">返回码：400</response>
+        // GET: api/My
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)] //描述响应类型
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// 根据id获取数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET: api/My/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// 获取数据
+        /// </summary>
+        /// <param name="value"></param>
+        // POST: api/My
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// 修改数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        // PUT: api/My/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="id"></param>
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ 使用Swagger/Swagger显示接口/SwaggerDemo/SwaggerDemo/Startup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace SwaggerDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            //Swagger
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {//API 信息和说明
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "An ASP.NET Core Web API for managing ToDo items", //描述
+                    TermsOfService = new Uri("https://example.com/terms"), //服务条款
+                    Contact = new OpenApiContact
+                    {//联系
+                        Name = "Example Contact",
+                        Url = new Uri("https://example.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {//许可证
+                        Name = "Example License",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+                //XML 注释 启用：<PropertyGroup><GenerateDocumentationFile>true</GenerateDocumentationFile></PropertyGroup>
+                // using System.Reflection;
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"; //获取程序集的简单名称。
+                // 基于XML注释文件为操作、参数和模式注入人性化的描述
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename)); //将多个字符串组合成一个路径。
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //Swagger
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"); //IIS或反向代理
+                    options.RoutePrefix = "api"; //string.Empty; //要在应用的根 (https://localhost:<port>/) 处提供 Swagger UI，请将 RoutePrefix 属性设置为空字符串（string.Empty）
+                    //options.InjectStylesheet("/swagger-ui/custom.css"); //自定义 UI 样式文件
+                    //options.InjectJavascript("/swagger-ui/zh_CN.js"); // 加载中文包
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
